3f3ae318fd78646b4c84edd232e5c28af5e46fa1
arthurcastro <arthurfjadecastro@gmail.com>
2023-06-02 20:50:41 -0300

    Create dialog simulator

--- src/App.js
+++ src/App.js
@@ -1,16 +1,73 @@
+import { useState } from 'react';
 import './App.css';
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import { useMatchesNotebooks, useMatchesSmartphone } from "./Components/Breakpoints"
+// import { Route, BrowserRouter, Routes, Outlet, useNavigate } from "react-router-dom";
+import { makeStyles, Theme, createStyles } from "@mui/material/styles";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import { AppBar, Grid, IconButton, Toolbar, Link } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import Slider from "react-slick";
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import "./slick-list.css"
+// import { Box, Typography } from "@mui/material";
+import { Email, Facebook, Twitter, LinkedIn } from "@mui/icons-material";
+import {SimulateDialog} from "./Components/Dialogs"
+
 
 
 function Home() { 
+  const [isOpenDialog, setIsOpenDialog] = useState(false);
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 2500,
+    slidesToShow: 1,
+    // slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    fade: true
+    // centerMode: true,
+  };
 
 
   const query = useMatchesSmartphone()
-  console.log(query)
+  // console.log(query)
   return (
     <>
-      <h1>hello world</h1>
+      <Box sx={{ flexGrow: 1 }} mx="auto" maxWidth="100%" style={{ width: "100%", overflow: "hidden", marginTop: 0 }}>
+        <AppBar position="absolute" style={{ background: "linear-gradient(to right, #005CA9, #54BBAB)" }}>
+          <Toolbar style={{ minHeight: "65px !important" }}>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1, display: "flex" }}>
+            <Button variant="outlined" onClick={() => setIsOpenDialog(true)}>
+              Simular
+            </Button>
+            </Typography>
+          </Toolbar>
+        </AppBar>
+        <Box mx="auto" maxWidth="100%" style={{ width: "100%", overflow: "hidden" }}>
+          <Slider pauseOnHover={false} {...settings} style={{ width: "100%", padding: 0, flex: 1, display: "flex" }}> 
+             <div style={{ padding: 0, display: "flex", flexDirection: "row" }}>
+              <img src="/Images/logo1.png" alt="Slide 2" style={{ height: "auto", marginRight: "32px", objectFit: "cover", width: "100%", maxWidth: 1280 }} />
+            </div>
+            <div style={{ padding: 0, display: "flex", flexDirection: "row" }}>
+              <img src="/Images/fundo24.png" alt="Slide 2" style={{ height: "auto", marginRight: "32px", objectFit: "cover", width: "100%" }} />
+            </div>
+          </Slider>
+          <div>
+          </div>
+        </Box>
+      </Box>
+      <Box sx={{ position: "fixed", bottom: 0, width: "100%", background: "linear-gradient(to right, #005CA9, #54BBAB)" }}>
+            <h1>footer</h1>
+        <Box p={3} style={{ background: "linear-gradient(to right, #005CA9, #54BBAB)" }}>
+          <h1>footer</h1> 
+        </Box>
+      </Box>
+      <SimulateDialog isOpen={isOpenDialog} setClose={setIsOpenDialog}/>
     </>
   );
 }
---
+++ src/Components/Dialogs/SimulateDialog.js
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import ListItemText from '@mui/material/ListItemText';
+import ListItem from '@mui/material/ListItem';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import CloseIcon from '@mui/icons-material/Close';
+import Slide from '@mui/material/Slide';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+function SimulateDialog({isOpen, setClose}) {
+  console.log(isOpen)
+
+
+  return (
+    <div>
+      <Dialog
+        fullScreen
+        open={isOpen}
+        onClose={() => setClose(false)}
+        TransitionComponent={Transition}
+      >
+        <AppBar sx={{ position: 'relative' }}>
+          <Toolbar>
+            <IconButton
+              edge="start"
+              color="inherit"
+              onClick={() => setClose(false)}
+              aria-label="close"
+            >
+              <CloseIcon />
+            </IconButton>
+            <Typography sx={{ ml: 2, flex: 1 }} variant="h6" component="div">
+              SimuladorCred CAIXA
+            </Typography>
+            <Button autoFocus color="inherit" onClick={() => setClose(false)}>
+              continuar
+            </Button>
+          </Toolbar>
+        </AppBar>
+        <List>
+          <ListItem button>
+            <ListItemText primary="Phone ringtone" secondary="Titania" />
+          </ListItem>
+          <Divider />
+          <ListItem button>
+            <ListItemText
+              primary="Default notification ringtone"
+              secondary="Tethys"
+            />
+          </ListItem>
+        </List>
+      </Dialog>
+    </div>
+  );
+}
+
+export default SimulateDialog;
