6421285ad2246a8ca50131f79c8c34fbad9b92a4
arthurcastro <arthurfjadecastro@gmail.com>
2023-06-02 19:44:56 -0300

    Define layout structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import { useMatchesNotebooks, useMatchesSmartphone } from "./Components/Breakpoints"
+
+
+function Home() { 
+
+
+  const query = useMatchesSmartphone()
+  console.log(query)
+  return (
+    <>
+      <h1>hello world</h1>
+    </>
+  );
+}
+
+
+
+function Layout() {
+  return (
+    <div>
+      <Home></Home>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+      </Routes>
+    </BrowserRouter >
   );
 }
 
---
+++ src/Components/Breakpoints/BreakpointsDefault.js
@@ -0,0 +1,6 @@
+const BreakpointsDefault = {
+  "mobile": "(max-width:959px)",
+  "desktop": "(min-width: 960px)",
+};
+
+export default BreakpointsDefault;
